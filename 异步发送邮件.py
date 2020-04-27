bdec9212cba2cc785a198cb62f4aa9b4c67ce60b
zhushixia <42405349+zhushixia@users.noreply.github.com>
2020-04-28 06:53:19 +0800

    modify

--- 异步发送邮件.py
+++ 异步发送邮件.py
@@ -10,14 +10,19 @@ import win32gui
 import win32ui
 from flask import current_app, render_template
 from flask_mail import Message
+from flask_restplus import Namespace, Resource, abort
+
+from info.modules.index.test import test
 import time
 
 
+apitest = Namespace("users", description="Users CURD api.")
 # todo = api.model('Todo', {
 #     'id': fields.Integer(readonly=True, description='The task unique identifier'),
 #     'task': fields.String(required=True, description='The task details')
 # })
 # ns = api.namespace('todos', description='TODO operations', path="/api")
+
 
 
 @apitest.route('/get')
@@ -27,7 +32,6 @@ class Zhushi(Resource):
         send_mail()
         return "ok"
 
-
 def send_async_email():
     outlook = win32.Dispatch('Outlook.Application')
     reciList = ['15868407338@163.com']
@@ -36,11 +40,8 @@ def send_async_email():
         mail_item.Recipients.Add(reciList[i])
         mail_item.Subject = 'Mail Test'
         mail_item.BodyFormat = 2  # 2: Html format
-        mail_item.HTMLBody = '''
-                <H2>Hello, This is a test mail~~.</H2>
-                Hello Guys. '''
-
-        # mail_item.Attachments.Add('d:\doc\IT Info.xlsx')
+        mail_item.Attachments.Add('F:\\test\\3.png')
+        mail_item.HTMLBody = "<div><img src='3.png' /></div>"
         mail_item.Send()
 
 
